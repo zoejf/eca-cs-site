57fdd588cd6092f9fb304d0ed3117d04e5552688
jhars <jjharlan06@gmail.com>
2015-11-18 17:38:47 -0800

    initial layout outline

---
+++ app/controllers/template_controller.rb
@@ -0,0 +1,13 @@
+class TemplateController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,42 @@
 Rails.application.routes.draw do
   
+	get '/delivery-schedule', to: 'search#index'
+
+	get '/search-emails', to: 'email#index' 
+
+
+
+  # get 'search/index'
+
+  get 'search/show'
+  get 'search/new'
+  get 'search/edit'
+
+  # get 'email/index' 
+  get 'email/show'
+  get 'email/new'
+  get 'email/edit'
+
+  # get 'template/index'
+  get '/template-emails', to: 'template#index'
+  get 'template/show'
+  get 'template/new'
+  get 'template/edit'
+
   root 'pages#index'
 
 end
+
+
+
+
+#         Prefix Verb URI Pattern               Controller#Action
+#    email_index GET  /email/index(.:format)    email#index
+#     email_show GET  /email/show(.:format)     email#show
+#      email_new GET  /email/new(.:format)      email#new
+#     email_edit GET  /email/edit(.:format)     email#edit
+# template_index GET  /template/index(.:format) template#index
+#  template_show GET  /template/show(.:format)  template#show
+#   template_new GET  /template/new(.:format)   template#new
+#  template_edit GET  /template/edit(.:format)  template#edit
+#           root GET  /                         pages#index
